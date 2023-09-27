6f218c60d429e9d14f92925c8d176dd5084231cc
Anton240896 <antonalipov791@gmail.com>
2023-09-27 19:09:44 +0300

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,6 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+// import { App } from 'components/App';
+import data from 'data/user.json'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const card = <div class="card" style={{width: '18rem',marginLeft:'50%'}}>
+<img src={data.avatar} class="card-img-top" style={{width:'100px'}} alt={data.username}/>
+<div class="card-body">
+  <h3 class="card-title">{data.username}</h3>
+  <p class="card-text">{data.tag}</p>
+  <p class="card-location">{data.location}</p>
+</div>
+</div>
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(card);
